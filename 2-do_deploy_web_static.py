de2a563cf6a25b15571c76d4f6ec4d0cf616c7ca
Phil <83282650+Philemon04@users.noreply.github.com>
2024-01-04 13:11:56 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
-""" Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy """
-
-from datetime import datetime
-from fabric.api import *
-import shlex
+"""Distributes an archive to your web servers, using the function do_deploy"""
+from fabric.contrib import files
+from fabric.api import env, put, run
 import os
 
-
-env.hosts = ['34.74.243.116', '3.239.3.209']
-env.user = "ubuntu"
+env.hosts = ['104.196.116.233', '54.165.130.77']
 
 
 def do_deploy(archive_path):
-    """ Deploys """
+    """Function for deploy"""
     if not os.path.exists(archive_path):
         return False
+
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dest))
         return True
     except:
         return False
